f30052d832580e4c34a3bb600d221d8f82a57b9d
binoysarker <binoy3412@gmail.com>
2017-10-03 10:30:48 +0600

    need to work with the product edit section

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -141,23 +141,34 @@ class ProductController extends Controller
             'product_file'   =>  'required|max:100'
 
         ]);
-        /*$products = new Product;
-        $products->category_id = $request->category_id;
-        $products->sub_category_id = $request->sub_category_id;
-        $products->productName = $request->productName;
-        $products->productBrief = $request->productBrief;
-        $products->productDescription = $request->productDescription;
-        $products->productPrice = $request->productPrice;
-        $products->Quantity = $request->Quantity;
-        $products->Condition = $request->Condition;
-        $products->BrandName = $request->BrandName;
-        $products->product_file = $request->product_file;*/
         $getId = $product['id'];
-        $products = Product::where('id',$getId);
-        $products->update(\request(['category_id','sub_category_id','productName','productBrief','productDescription','productPrice','Quantity','Condition','BrandName','product_file']));
+        $products = Product::find($getId);
 
-        session()->flash('message','Data Updated Successfuly');
-        return redirect('/admin');
+        if ($product['product_file'] == $request->product_file){
+            $products->update(\request(['category_id','sub_category_id','productName','productBrief','productDescription','productPrice','Quantity','Condition','BrandName','product_file']));
+            session()->flash('message','Data Updated Successfuly');
+            return redirect('/admin');
+        }
+        if($product['product_file'] != $request->product_file){
+            unlink("".$request->product_file);
+            $image = $request->file('product_file');
+            if ($image) {
+                $imageName = str_random(20);
+                $ext = strtolower($image->getClientOriginalExtension());
+                $full_image_name = $imageName . '.' . $ext;
+                $upload_path = 'upload/';
+                $image_url = $upload_path . $full_image_name;
+                $success = $image->move($upload_path, $full_image_name);
+                if ($success) {
+                    $request->product_file = $image_url;
+                    $products->update(\request(['category_id','sub_category_id','productName','productBrief','productDescription','productPrice','Quantity','Condition','BrandName','product_file']));
+                    session()->flash('message', 'Data Inserted Successfully');
+                    return redirect('/admin');
+                } else {
+                    echo 'No file uploaded';
+                }
+            }
+        }
     }
 
     /**
